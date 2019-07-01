c257e04c6a11e9e414526e9d04ed641731ae368f
181192 <k@kalli.no>
2019-07-01 14:31:09 +0200

    Add resource_group_name and container_name to output

--- modules/az_storage_account/outputs.tf
+++ modules/az_storage_account/outputs.tf
@@ -2,3 +2,11 @@ output "account_access_key" {
   description = "The storage account primary access key"
   value = "${azurerm_storage_account.tf_sa.primary_access_key}"
 }
+
+output "resource_group_name" {
+  value = "${azurerm_resource_group.tf_rg.name}"
+}
+
+output "container_name" {
+  value = "${azurerm_storage_container.tf_sc.name}"
+}
