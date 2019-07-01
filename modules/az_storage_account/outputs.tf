7bd14f8dae61f5a29f4c9059b14d03159a299859
181192 <k@kalli.no>
2019-07-01 13:52:25 +0200

    Fix refrence to primary_access_key

--- modules/az_storage_account/outputs.tf
+++ modules/az_storage_account/outputs.tf
@@ -1,3 +1,4 @@
 output "account_access_key" {
-  value = "${azurerm_storage_account.tf_sc.primary_access_key}"
+  description = "The storage account primary access key"
+  value = "${azurerm_storage_account.tf_sa.primary_access_key}"
 }
