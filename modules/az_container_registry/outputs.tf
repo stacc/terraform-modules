118280aee3382be0a6e7feaf66e6f359ef967c1d
Audun Meltzer <audun.meltzer@stacc.com>
2019-07-02 10:52:02 +0200

    export av acr object fra container_registry modul

--- modules/az_container_registry/outputs.tf
+++ modules/az_container_registry/outputs.tf
@@ -2,3 +2,7 @@ output "login_server" {
   description = "URL of ACR"
   value = "${azurerm_container_registry.acr.login_server}"
 }
+output "acr" {
+  description = "ACR object"
+  value = "${azurerm_container_registry.acr}"
+}
