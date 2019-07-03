24ccd7f2c2dfef8530f0d476a5bf6f88ae40eb27
181192 <k@kalli.no>
2019-07-03 10:20:55 +0200

    Add sa suffix to storage account resource group

--- modules/az_storage_account/main.tf
+++ modules/az_storage_account/main.tf
@@ -3,7 +3,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "${var.name}-${var.environment}-rg"
+  name     = "${var.name}-${var.environment}-sa-rg"
   location = "${var.location}"
 
   tags = {
