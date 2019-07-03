25a39e00d07ab493134bf08affb639b68fa59c2b
181192 <k@kalli.no>
2019-07-03 14:38:28 +0200

    Add storage account name output

--- modules/az_storage_account/outputs.tf
+++ modules/az_storage_account/outputs.tf
@@ -1,4 +1,8 @@
-output "account_access_key" {
+output "name" {
+  value = "${var.sa_name}"
+}
+
+output "access_key" {
   description = "The storage account primary access key"
   value = "${azurerm_storage_account.storage_account.primary_access_key}"
 }
