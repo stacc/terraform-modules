43709f6baf5977928af20d508c2e594323ea769a
181192 <k@kalli.no>
2019-07-03 14:27:55 +0200

    Extract resource group from module to make more generic

--- modules/az_storage_account/outputs.tf
+++ modules/az_storage_account/outputs.tf
@@ -3,10 +3,6 @@ output "account_access_key" {
   value = "${azurerm_storage_account.storage_account.primary_access_key}"
 }
 
-output "resource_group_name" {
-  value = "${azurerm_resource_group.resource_group.name}"
-}
-
 output "container_name" {
   value = "${azurerm_storage_container.storage_container.name}"
 }
