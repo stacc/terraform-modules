18ec71e67685ce071b2f385656cb39e52e9372a2
181192 <k@kalli.no>
2019-07-04 09:28:45 +0200

    Add primary connection string to output

--- modules/az_storage_account/outputs.tf
+++ modules/az_storage_account/outputs.tf
@@ -10,3 +10,7 @@ output "access_key" {
 output "container_name" {
   value = "${azurerm_storage_container.storage_container.name}"
 }
+
+output "primary_connection_string" {
+  value = "${azurerm_storage_account.storage_account.primary_connection_string}"
+}
