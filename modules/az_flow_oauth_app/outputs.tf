e267835871265947a759a08ce4e5a4c0406a95e7
Audun Meltzer <audun.meltzer@stacc.com>
2019-07-05 10:16:15 +0200

    oppdaterte feile ref

--- modules/az_flow_oauth_app/outputs.tf
+++ modules/az_flow_oauth_app/outputs.tf
@@ -5,10 +5,10 @@ output "flow_password" {
 
 output "flow_app_id" {
   description = "The grafana application id"
-  value       = "${azuread_application.grafana.application_id}"
+  value       = "${azuread_application.flow.application_id}"
 }
 
 output "flow_client_id" {
   description = "The grafana client id"
-  value       = "${azuread_service_principal.grafana_client.id}"
+  value       = "${azuread_service_principal.flow_client.id}"
 }
