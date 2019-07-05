f270d2580c0b41ea8d6e849de6a3a713e5c8a70c
Audun Meltzer <audun.meltzer@stacc.com>
2019-07-05 11:54:45 +0200

    fjerner spesialtegn i passord

--- modules/az_flow_oauth_app/main.tf
+++ modules/az_flow_oauth_app/main.tf
@@ -32,7 +32,7 @@ resource "azuread_service_principal_password" "flow_client_password" {
 
 resource "random_string" "application_client_password" {
   length  = 16
-  special = true
+  special = false
 
   keepers = {
     service_principal = "${azuread_service_principal.flow_client.id}"
