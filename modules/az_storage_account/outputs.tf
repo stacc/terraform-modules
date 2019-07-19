002f88e27babf3628ddc6d8aa35a0bab7f6367d0
181192 <k@kalli.no>
2019-07-19 08:00:13 +0200

    Fix output variable name

--- modules/az_storage_account/outputs.tf
+++ modules/az_storage_account/outputs.tf
@@ -7,7 +7,7 @@ output "access_key" {
   value = "${azurerm_storage_account.storage_account.primary_access_key}"
 }
 
-output "container_name" {
+output "container_names" {
   value = "${azurerm_storage_container.storage_container.*.name}"
 }
 
