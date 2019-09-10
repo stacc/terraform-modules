ba02a1cbaa3651d8451b7047251c66ba337c6c1b
181192 <k@kalli.no>
2019-09-10 10:44:32 +0200

    Add deafult role assigments
    Assign cluster_admins and cluster_developers security groups Reader and Azure Kubernetes Service Cluster User Role roles

--- modules/az_kubernetes/iam.tf
+++ modules/az_kubernetes/iam.tf
@@ -19,3 +19,27 @@ resource "azuread_group" "cluster_admins" {
 resource "azuread_group" "cluster_developers" {
   name = "s_${var.name}-${var.environment}_cluster_developers"
 }
+
+resource "azurerm_role_assignment" "cluster_admin_aks" {
+  scope                = "${var.resource_group.id}"
+  role_definition_name = "Azure Kubernetes Service Cluster User Role"
+  principal_id         = "${azuread_group.cluster_admins.id}"
+}
+
+resource "azurerm_role_assignment" "cluster_admin_reader" {
+  scope                = "${var.resource_group.id}"
+  role_definition_name = "Reader"
+  principal_id         = "${azuread_group.cluster_admins.id}"
+}
+
+resource "azurerm_role_assignment" "cluster_developers_aks" {
+  scope                = "${var.resource_group.id}"
+  role_definition_name = "Azure Kubernetes Service Cluster User Role"
+  principal_id         = "${azuread_group.cluster_developers.id}"
+}
+
+resource "azurerm_role_assignment" "cluster_developers_reader" {
+  scope                = "${var.resource_group.id}"
+  role_definition_name = "Reader"
+  principal_id         = "${azuread_group.cluster_developers.id}"
+}
