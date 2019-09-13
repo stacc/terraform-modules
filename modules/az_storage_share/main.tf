efe9a33142444975866b4b05e2959d94ebde22c0
Audun Meltzer <audun.meltzer@stacc.com>
2019-09-13 14:12:08 +0200

    moved rg defenition outside module

--- modules/az_storage_share/main.tf
+++ modules/az_storage_share/main.tf
@@ -1,15 +1,6 @@
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.rg_name}"
-  location = "${var.location}"
-
-  tags = {
-    environment = "${var.environment}"
-  }
-}
-
 resource "azurerm_storage_account" "sa" {
   name                     = "${var.sa_name}"
-  resource_group_name      = "${azurerm_resource_group.rg.name}"
+  resource_group_name      = "${var.rg_name}"
   location                 = "${var.location}"
   account_tier             = "${var.account_tier}"
   account_replication_type = "${var.account_replication_type}"
