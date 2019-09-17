5c99e194935457f7c260218384bbeec25eb4fb02
Audun Meltzer <audun.meltzer@stacc.com>
2019-09-17 14:19:43 +0200

    Update support for assign sp azuread group

--- modules/az_kubernetes/outputs.tf
+++ modules/az_kubernetes/outputs.tf
@@ -1,3 +1,6 @@
 output "kubernetes_cluster" {
   value = "${azurerm_kubernetes_cluster.kubernetes}"
 }
+output "kubernetes_cluster_admin_group_id" {
+  value = "${azuread_group.cluster_admins.id}"
+}
