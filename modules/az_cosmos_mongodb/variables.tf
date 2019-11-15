54bd68f4b953ede21d56665bf5ceddb88070a1c7
181192 <k@kalli.no>
2019-11-15 13:41:36 +0100

    Upgrade to 0.12.14 syntax

--- modules/az_cosmos_mongodb/variables.tf
+++ modules/az_cosmos_mongodb/variables.tf
@@ -20,10 +20,12 @@ variable "consistency_level" {
   description = "The Consistency Level to use for this CosmosDB Account - can be either BoundedStaleness, Eventual, Session, Strong or ConsistentPrefix"
   default     = "BoundedStaleness"
 }
+
 variable "max_interval_in_seconds" {
   description = "When used with the Bounded Staleness consistency level, this value represents the time amount of staleness (in seconds) tolerated. Accepted range for this value is 5 - 86400 (1 day). Defaults to 5. Required when consistency_level is set to BoundedStaleness"
   default     = 10
 }
+
 variable "max_staleness_prefix" {
   description = "When used with the Bounded Staleness consistency level, this value represents the number of stale requests tolerated. Accepted range for this value is 10 – 2147483647. Defaults to 100. Required when consistency_level is set to BoundedStaleness"
   default     = 200
@@ -36,18 +38,22 @@ variable "account_name_suffix" {
 
 variable "database_names" {
   description = "List of the database names"
-  type        = list(string)
-  default     = ["test"]
+
+  # type        = list(string)
+  default = ["test"]
 }
 
 variable "unique_key" {
   default = "uniqueKey"
 }
 
-variable "failover_location" {}
+variable "failover_location" {
+}
 
 variable "capabilities" {
   default = "EnableAggregationPipeline"
 }
 
-variable "kubernetes_cluster" {}
+variable "kubernetes_cluster" {
+}
+
