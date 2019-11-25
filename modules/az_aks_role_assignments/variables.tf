e45a8d0780464aff9040d8fb4e49bff9f67e91d6
181192 <k@kalli.no>
2019-11-25 16:47:55 +0100

    Add az_aks_role_assignments module

---
+++ modules/az_aks_role_assignments/variables.tf
@@ -0,0 +1,19 @@
+variable "name" {
+  description = "The prefix of the resources"
+  default     = "stacc"
+}
+
+variable "environment" {
+  description = "The environment of the resources"
+  default     = "test-1"
+}
+
+variable "aks_rg_id" {}
+
+variable "db_rg_id" {}
+
+variable "public_ip_rg_id" {}
+
+variable "sa_rg_id" {}
+
+variable "aks_sp_object_id" {}
