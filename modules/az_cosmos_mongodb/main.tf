f84d27a0b1768b656a9b16949c2bee11f33b0893
181192 <k@kalli.no>
2019-11-29 12:57:27 +0100

    Fix kubernetes credentials

--- modules/az_cosmos_mongodb/main.tf
+++ modules/az_cosmos_mongodb/main.tf
@@ -39,10 +39,10 @@ resource "azurerm_cosmosdb_mongo_database" "database" {
 provider "kubernetes" {
   version                = "1.7"
   load_config_file       = false
-  host                   = var.kubernetes_cluster.kube_admin_config.0.host
-  client_certificate     = base64decode(var.kubernetes_cluster.kube_admin_config.0.client_certificate)
-  client_key             = base64decode(var.kubernetes_cluster.kube_admin_config.0.client_key)
-  cluster_ca_certificate = base64decode(var.kubernetes_cluster.kube_admin_config.0.cluster_ca_certificate)
+  host                   = var.kubernetes_cluster.kube_config.0.host
+  client_certificate     = base64decode(var.kubernetes_cluster.kube_config.0.client_certificate)
+  client_key             = base64decode(var.kubernetes_cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(var.kubernetes_cluster.kube_config.0.cluster_ca_certificate)
 }
 
 resource "kubernetes_secret" "database_secret" {
--- modules/az_mysql/main.tf
+++ modules/az_mysql/main.tf
@@ -38,14 +38,10 @@ resource "azurerm_mysql_server" "mysql" {
 provider "kubernetes" {
   version          = "1.7"
   load_config_file = false
-  host             = var.kubernetes_cluster.kube_admin_config[0].host
-  client_certificate = base64decode(
-    var.kubernetes_cluster.kube_admin_config[0].client_certificate,
-  )
-  client_key = base64decode(var.kubernetes_cluster.kube_admin_config[0].client_key)
-  cluster_ca_certificate = base64decode(
-    var.kubernetes_cluster.kube_admin_config[0].cluster_ca_certificate,
-  )
+  host             = var.kubernetes_cluster.kube_config.0.host
+  client_certificate = base64decode(var.kubernetes_cluster.kube_config.0.client_certificate)
+  client_key = base64decode(var.kubernetes_cluster.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(var.kubernetes_cluster.kube_config.0.cluster_ca_certificate)
 }
 
 resource "kubernetes_secret" "database_secret" {
